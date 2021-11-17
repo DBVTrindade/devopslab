654e1768446ad4fddd7b3692acca6c55962e6473
DBVTrindade <75432951+LKMTec@users.noreply.github.com>
2021-11-17 19:52:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Customizado"
+    return "Hello World Customizado"
 
 if __name__ == '__main__':
     app.run()
