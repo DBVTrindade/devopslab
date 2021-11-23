4a12d3cd4c1447c58b3cc1176778a123aa194430
DBVTrindade <75432951+LKMTec@users.noreply.github.com>
2021-11-23 12:04:58 -0300

    Teste

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Laboratório Pipeline DevOps 2"
 
 @app.route('/bug')                                                                                                                                
 def bad():                                                                                                                                        
